b3977cb51f266d28bbee3bf94449c46ef7a0fa5a
exqjk78 <purov.bordan@gmail.com>
2024-06-20 14:34:00 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,6 @@
+example ='abcde' # 1 задание
+print(example[0]) # 2 задание
+print(example[-1]) # 3 задание
+print(example[2:5]) # 4 задание
+print(example[::-1]) # 5 задание
+print(example[::2]) # 6 задание
